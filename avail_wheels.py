1b1ce66e0a2dfed34ad96cdc4e6980fd0a5ed7da
Charles Coulombe <charles.coulombe@calculquebec.ca>
2018-09-21 00:09:42 -0400

    Bump version to 1.0.1

--- avail_wheels.py
+++ avail_wheels.py
@@ -9,7 +9,7 @@ from tabulate import tabulate
 from distutils.version import LooseVersion
 from itertools import product
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 WHEELHOUSE = os.environ.get("WHEELHOUSE", "/cvmfs/soft.computecanada.ca/custom/python/wheelhouse")
 PYTHONS_DIR = os.environ.get("PYTHONS_DIR", "/cvmfs/soft.computecanada.ca/easybuild/software/2017/Core/python")
