7727464ebee33053e90e016b49d6345cd1de4a3c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-09-08 15:13:56 -0400

    Bump version to 3.0.0

--- avail_wheels.py
+++ avail_wheels.py
@@ -19,7 +19,7 @@ import signal
 # default UNIX signal handling for SIGPIPE globally.
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
-__version__ = "3.0.0.dev"
+__version__ = "3.0.0"
 
 env = RuntimeEnvironment()
 
